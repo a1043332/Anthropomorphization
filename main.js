ac9405bf9655aa2c222aa317283fb941c3d4a328
HDC <a1043332@mail.nuk.edu.tw>
2021-01-21 00:18:50 +0800

    rrr

--- main.js
+++ main.js
@@ -40,6 +40,24 @@ function sayNo() {
 }
 
 
+function nextStep1(){
+    var answerArea = document.querySelector('.answerArea');
+    var mainText = document.querySelector('.textArea');
+    answerArea.innerHTML = '<button class="btn btn-success btnNext" onclick="nextStep2()">下一步</button>'
+    mainText.innerHTML = ' <div class="topTitle">三種購物方式的範例</div><div class="examplePic"><img src="pictures/example1.png" class="exampleImg" alt=""></div><div class="examplePic"><img src="pictures/example2.png" class="exampleImg" alt=""></div><div class="examplePic"><img src="pictures/example3.png" class="exampleImg" alt=""></div><div class="clear"></div>';
+
+}
+function nextStep2(){
+    var answerArea = document.querySelector('.answerArea');
+    var mainText = document.querySelector('.textArea');
+    answerArea.innerHTML = '<button class="btn btn-success btnNext" onclick="nextStep3()">下一步</button>'
+    mainText.innerHTML = '<div class="topTitle">實驗流程</div><div class="stepDescription"><ul class="list-group"><li class="list-group-item">注視銀幕的中間十字標記，專心參與實驗 </li><li class="list-group-item">介紹購物助理</li><li class="list-group-item">購物助理介紹推薦的產品</li><li class="list-group-item">針對購物助理回答相關的問題</li><li class="list-group-item">購物助理介紹產品及價格</li><li class="list-group-item">評估並回答你的購買意願 </li><li class="list-group-item">重複以上步驟，直到完成實驗</li><li class="list-group-item">接著下面就開始實驗</li></ul></div>';
+
+}
+function nextStep3(){
+    window.location.href="recommendTest.html"
+}
+
 
 
 
